a9aa4e45fc30e53a050096bad30fd3298152dc4e
kiransp <kiransp82@gmail.com>
2016-05-12 11:58:09 +0530

    aaded achievemets dynamicaly with badges

--- app/scripts/controllers/achievementsCtrl.js
+++ app/scripts/controllers/achievementsCtrl.js
@@ -9,6 +9,80 @@
  */
 angular.module('introMeApp')
     .controller('achievementsCtrl', function ($scope) {
-        
-     
+        $scope.achievements = [
+            {
+                year: 2012,
+                achievementsDesc: [
+                    {
+                        achievementName: "Race",
+                        description: "hi hello"
+                    },
+                    {
+                        achievementName: "Race",
+                        description: "hi hello"
+                    },
+                    {
+                        achievementName: "Race",
+                        description: "hi hello"
+                    }
+                ]
+            },
+            {
+                year: 2011,
+                achievementsDesc: [
+                    {
+                        achievementName: "Race",
+                        description: "hi hello"
+                    },
+                    {
+                        achievementName: "Race",
+                        description: "hi hello"
+                    },
+                    {
+                        achievementName: "Race",
+                        description: "hi hello"
+                    }
+                ]
+            },
+            {
+                year: 2013,
+                achievementsDesc: [
+                    {
+                        achievementName: "Race",
+                        description: "hi hello"
+                    },
+                    {
+                        achievementName: "Race",
+                        description: "hi hello"
+                    },
+                    {
+                        achievementName: "Race",
+                        description: "hi hello"
+                    }
+                ]
+            },
+            {
+                year: 2010,
+                achievementsDesc: [
+                    {
+                        achievementName: "Race",
+                        description: "hi hello"
+                    },
+                    {
+                        achievementName: "Race",
+                        description: "hi hello"
+                    },
+                    {
+                        achievementName: "Race",
+                        description: "hi hello"
+                    }
+                ]
+            }
+        ];
+
+        // use slice() to copy the array and not just make a reference
+        $scope.achievements = $scope.achievements.slice(0);
+        $scope.achievements.sort(function (a, b) {
+            return a.year - b.year;
+        });
     });
