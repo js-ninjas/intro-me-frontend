2c387ff983a727268ba8158916c37f94aa01e8ba
kiransp <kiransp82@gmail.com>
2016-05-13 16:31:03 +0530

    added functinaloty for achievements

--- app/scripts/controllers/achievementsCtrl.js
+++ app/scripts/controllers/achievementsCtrl.js
@@ -12,30 +12,45 @@ angular.module('introMeApp')
         $scope.headingBgColors = ['#F8DDA1', '#B9A593'];
         $scope.contentBgColors = ['#81C6DD'];
 
-        $scope.achievements = [{
-            year: 2012,
-            achievementsDesc: [
-                {
-                    achievementName: "Race",
-                    description: "hi hello"
-                },
-                {
-                    achievementName: "Race",
-                    description: "hi hello"
-                },
-                {
-                    achievementName: "Race",
-                    description: "hi hello"
-                }
-            ]
-        }];
+        $scope.achievements = [
+            {
+                "year": 2016,
+                "achievementsDesc": [
+                    {
+                        "achievementName": "asd",
+                        "description": "asd",
+                        "$$hashKey": "object:69"
+                    },
+                    {
+                        "achievementName": "asd",
+                        "description": "asd",
+                        "$$hashKey": "object:74"
+                    }
+                ]
+            },
+            {
+                "year": 2017,
+                "achievementsDesc": [
+                    {
+                        "achievementName": "asd",
+                        "description": "asd",
+                        "$$hashKey": "object:69"
+                    },
+                    {
+                        "achievementName": "asd",
+                        "description": "asd",
+                        "$$hashKey": "object:74"
+                    }
+                ]
+            }
+        ];
 
         // use slice() to copy the array and not just make a reference
-        $scope.achievements = $scope.achievements.slice(0);
-        $scope.achievements.sort(function (a, b) {
-            return a.year - b.year;
-        });
-
+        /*       $scope.achievements = $scope.achievements.slice(0);
+         $scope.achievements.sort(function (a, b) {
+         return a.year - b.year;
+         });
+         */
 
         $scope.colorPicker = function (index, isHeading) {
 //if odd pos return 0
@@ -50,18 +65,109 @@ angular.module('introMeApp')
 
 
         $scope.addAchievement = function () {
-            console.log("achievementName :" + $scope.achievementName)
-            console.log("achievementDesc :" + $scope.achievementDesc)
+            var year = new Date($scope.year).getFullYear();
+
             if ($scope.achievementName != null && $scope.achievementDesc != null) {
                 var temp = {};
-                temp['achievementName'] = $scope.achievementName;
-                temp['description'] = $scope.achievementDesc;
-                $scope.hobbies.push(temp);
-                $scope.newHobby = null;
-                $scope.newDesc = null;
+                /*   temp['achievementName'] = $scope.achievementName;
+                 temp['description'] = $scope.achievementDesc;*/
+                var pos = -1;
+                console.log("ARRY LENGTH " + $scope.achievements.length)
+                if ($scope.achievements.length != 0) {
+                    /* $scope.achievements.forEach(function (achievement, i) {
+                     console.log("ITERATION VALUE " + i);
+                     console.log("ITERATION VALUE " + i);
+
+                     /!*            if (achievement.year == year) {
+                     console.log("FOUND YEAR " + year)
+                     console.log("achievement BEFORE" + JSON.stringify($scope.achievements))
+                     console.log("a$scope.achievements['year'] " + JSON.stringify($scope.achievements[i]['achievementsDesc']))
+                     $scope.achievements[i]['achievementsDesc'].push(temp);
+                     console.log("FINAL OBJ " + JSON.stringify($scope.achievements))
+
+                     }
+                     else {
+                     console.log(" YEAR NOT FOUND " + year)
+                     var newObj = {};
+                     newObj['year'] = year;
+                     newObj['achievementsDesc'] = [temp];
+                     $scope.achievements.push(newObj);
+                     }*!/
+                     })*/
+                    for (var i = 0; i < $scope.achievements.length; i++) {
+                        if ($scope.achievements[i].year == year) {
+                            pos = i;
+                        }
+                    }
+
+                    if (pos == -1) {
+                        console.log("YEAR DOES NOT EXISTS");
+                        var newObj = {};
+                        var achievementsDesc = []
+                        //newObj['year'] = (year);
+                        newObj['achievementName'] = $scope.achievementName;
+                        newObj['description'] = $scope.achievementDesc;
+                        achievementsDesc.push(newObj);
+                        var tempObj = {};
+                        tempObj['year'] = year;
+                        tempObj['achievementsDesc'] = achievementsDesc;
+                        $scope.achievements.push(tempObj);
+                    }
+                    else {
+                        temp["achievementName"] = $scope.achievementName;
+                        temp["description"] = $scope.achievementDesc;
+                        //console.log("TEMP OBJ   " + JSON.stringify(temp))
+                        $scope.achievements[pos]['achievementsDesc'].push(temp);
+                    }
+                }
+                else {
+                    console.log(" FIRST RECORD " + year)
+                    var newObj = {};
+                    var achievementsDesc = []
+                    //newObj['year'] = (year);
+                    newObj['achievementName'] = $scope.achievementName;
+                    newObj['description'] = $scope.achievementDesc;
+                    achievementsDesc.push(newObj);
+                    var tempObj = {};
+                    tempObj['year'] = year;
+                    tempObj['achievementsDesc'] = achievementsDesc;
+                    $scope.achievements.push(tempObj);
+                    console.log("NEW OBJ    " + JSON.stringify($scope.achievements))
+
+                }
+
             }
             else {
-                alert("Fill the form");
+                alert("All fields are mandatory");
             }
-        }
+        };
+
+
+        $scope.clear = function () {
+            $scope.dt = null;
+        };
+
+
+        $scope.minDate = $scope.minDate ? null : new Date("01/01/1950");
+        var d = new Date();
+        var n = d.getFullYear();
+        n = n + 1;
+        var m = "31/December/" + n;
+        $scope.maxDate = new Date(m);
+
+
+        $scope.open = function ($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.opened = true;
+        };
+        $scope.dateOptions = {
+            formatYear: 'yy',
+            startingDay: 1,
+            minMode: 'month'
+        };
+
+        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+        $scope.format = $scope.formats[0];
+
     });
