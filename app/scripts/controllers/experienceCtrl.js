eb5fbd55847153ca299dc3703f1a9ef6d18268e0
admin <admin@hussains-MacBook-Pro.local>
2016-05-17 11:54:29 +0530

    created add delete and edit fuctions for experience

--- app/scripts/controllers/experienceCtrl.js
+++ app/scripts/controllers/experienceCtrl.js
@@ -8,12 +8,15 @@
  * Controller of the introMeApp
  */
 angular.module('introMeApp')
-  .controller('experienceCtrl', function ($scope) {
+  .controller('experienceCtrl', function ($scope,$route) {
        
-       
+     $scope.project="Add";
+     $scope.projectBtn="Save";
+     $scope.childIndex=null;
+     $scope.parentIndex=null;
+     $scope.maxDateProject=null;
+     $scope.minDateProject=null;
     //Experiance JSON
-
-
     $scope.experiences =[
 	  {
 	    company: "TCS",
@@ -92,41 +95,196 @@ angular.module('introMeApp')
 	  }
 	];
 
-	 if($scope.experiences!=null)
-    {
-	$scope.hideShow=true;
-    $scope.editShow=false;
-    }
-    else
-    {
-    	$scope.hideShow=false;
-    $scope.editShow=true;
-    	
-    }
-    // Experience JSON
-     $scope.personInfo={company: "TCS" ,contactNo: "9876543210", email: "abcdEF@gmail.com", fName: "Mohammad",homeTown:"Hazaribagh",lName:"Hussain",location:"India",mDate:"May-2001",iDate:"May-2003",gDate:"July-2007",pDate:"August-2009"};
+	$scope.load=function()
+	{
+		$route.reload();
+	};
 
-$scope.showEdit=function()
-    {
-         $scope.hideShow=false;
-         $scope.editShow=true;
-    };
-    
-    $scope.hideEdit=function()
-    {	
-    	 if($scope.experiences==null)
-		    {
-			$scope.hideShow=true;
-		    $scope.editShow=false;
-			}
-		    else
-		    {
-       			 $scope.editShow=!$scope.editShow;
-       			 $scope.hideShow=!$scope.hideShow;
-       		}
-       
-    };
+	
+// add edit projects
+
+$scope.addProject=function(index){
+	$scope.project="Add";
+	$scope.projectBtn="Save";
+	$scope.childIndex=null;
+	$scope.parentIndex=index;
+    var d= $scope.experiences[index].from;
+    var dd= $scope.experiences[index].to;
+ $scope.minDateProject = new Date(d);
+	$scope.maxDateProject= new Date(dd);
+   
+    //Experiance JSON
+    console.log($scope.maxDateProject);
+    console.log($scope.minDateProject);
+	console.log(index);
+};
+
+$scope.editProject=function(index,parent){
+ $scope.project="Edit";
+ $scope.projectBtn="Update";
+ $scope.childIndex=index;
+ $scope.parentIndex=parent;
+
+ var d= $scope.experiences[parent].from;
+    var dd= $scope.experiences[parent].to;
 
 
+	$scope.minDateProject = $scope.minDateProject ? null : new Date("01/March/2011");
+	$scope.maxDateProject=$scope.minDateProject ? null : new Date(dd);
+   
+
+ $scope.projectData=$scope.experiences[parent].projects[index];
+ console.log(index,parent);
+};
+// Saving and updating projects
+$scope.saveUpdateProject=function(data)
+{
+ 	console.log($scope.childIndex,$scope.parentIndex);
+	 if($scope.childIndex==null)
+	 {
+	 	// Save New values 
+	 	$scope.experiences[$scope.parentIndex].projects.push(data);
+	 	console.log("Savin Values !!!!");
+	 }
+	 else
+	 {
+	 	// update values
+	 	$scope.experiences[$scope.parentIndex].projects[$scope.childIndex]=data;
+	 	console.log("Updating Values !!!!");
+	 }
+ 	$scope.childIndex=null;
+ 	$scope.parentIndex=null;
+};
+
+//Project experiance edit and save 
+$scope.addExperience=function()
+{
+	$scope.project="Add";
+ 	$scope.projectBtn="Save";
+};
+
+$scope.editExperiance=function(index)
+{	
+	$scope.parentIndex=index;
+	$scope.project="Edit";
+ 	$scope.projectBtn="Update";
+ 	console.log(index);
+ 	$scope.experinceData=$scope.experiences[index];
+
+
+};
+$scope.saveUpdateExprience=function(data)
+{
+	if($scope.parentIndex==null)
+	{
+		// Save New Experience 
+		var temp={};
+		temp['company']=data.company;
+		temp['from']=data.from;
+		temp['to']=data.to;
+		temp['projects']=[];
+		$scope.experiences.push(temp);
+		console.log(temp);
+		console.log("Savin Values !!!!");
+	}
+	else
+	{
+		// update Experience
+		$scope.experiences[$scope.parentIndex]=data;
+	 	console.log("Updating Values !!!!");
+	 	console.log(data);
+	}
+	$scope.childIndex=null;
+ 	$scope.parentIndex=null;
+};
+
+
+// DELETE PROJECT OR EXPERIANCE
+$scope.deleteExperiance=function(index)
+{
+	$scope.childIndex=null;
+ 	$scope.parentIndex=index; 
+    console.log(index);
+};
+$scope.deleteProject=function(index,parent)
+{
+	$scope.childIndex=index;
+ 	$scope.parentIndex=parent; 
+};
+$scope.delete=function()
+{
+	if($scope.childIndex==null)
+	{
+		// Delete  whole Experince
+		console.log($scope.parentIndex);
+	 	console.log("Deleting experiance !!!!");
+	 	if ($scope.parentIndex > -1)
+	 	{
+			$scope.experiences.splice($scope.parentIndex, 1);
+		}
+
+	 	//delete $scope.experiences[$scope.parentIndex];
+	 	console.log($scope.experiences);
+
+	 	// $scope.load();
+	 	//$route.reload();
+	}
+	else
+	{
+		// Delete Project
+		console.log($scope.childIndex,$scope.parentIndex);
+	 	console.log("deleting Project !!!!");
+
+	 	if ($scope.childIndex > -1)
+	 	{
+			$scope.experiences[$scope.parentIndex].projects.splice($scope.childIndex, 1);
+		}
+	 	// delete $scope.experiences[$scope.parentIndex].projects[$scope.childIndex];
+	 	console.log($scope.experiences[$scope.parentIndex].projects);
+
+	 	// $scope.load();
+	 	//$route.reload();
+	}
+	$scope.childIndex=null;
+ 	$scope.parentIndex=null;
+
+};
+
+
+ //datepicker start
+
+        $scope.clear = function () {
+            $scope.dt = null;
+        };
+
+
+        $scope.minDate = $scope.minDate ? null : new Date("01/01/1950");
+        var d = new Date();
+        var n = d.getFullYear();
+        n = n + 1;
+        var m = "31/December/" + n;
+        $scope.maxDate = new Date(m);
+
+
+        $scope.open = function ($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.opened = true;
+        };
+        $scope.open1 = function ($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.opened1 = true;
+        };
+        $scope.dateOptions = {
+            formatYear: 'yyyy',
+            startingDay: 1,
+            minMode: 'month'
+        };
+
+        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+        $scope.format = $scope.formats[0];
+
+ //////////////date end
 
   });
